6fdf7090c3772fe38637bad1ff9d61c824287924
ejaman <leegm042@gmail.com>
2022-08-01 16:04:09 +0900

    초기세팅, 로그인, 회원가입 컴포넌트 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,16 @@
 import React from "react";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// TODO: 나중에 질문하기 -> 타입스크립트 사용때문에 생기는 에러
+// import { ReactQueryDevtools } from "react-query/devtools";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      {/* <ReactQueryDevtools initialIsOpen={true} /> */}
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
