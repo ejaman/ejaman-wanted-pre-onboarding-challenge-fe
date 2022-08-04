116c863c1f8d756264c2795031c998f7c0bd4759
ejaman <leegm042@gmail.com>
2022-08-05 01:08:01 +0900

    feat: header style

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { onhandleLogout } from "../apis";
 
@@ -11,11 +12,26 @@ const Header = () => {
     navigate("/");
   };
   return (
-    <header style={{ display: "flex" }}>
-      <h3>header todo application</h3>
-      {token && <button onClick={onLogoutClick}>Logout</button>}
-    </header>
+    <HeaderDiv>
+      <h3>todo application</h3>
+      {token && <LogoutBtn onClick={onLogoutClick}>Logout</LogoutBtn>}
+    </HeaderDiv>
   );
 };
+const HeaderDiv = styled.header`
+  display: flex;
+  padding: 1rem 5rem;
+  align-items: center;
+`;
+const LogoutBtn = styled.button`
+  margin-left: auto;
+  font-size: 1.1rem;
+  font-weight: bold;
+  background: none;
+  border: none;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
 
 export default Header;
